3984d358bb7e05beb980631466b713f3ead47bb6
pulosevvy <107025377+pulosevvy@users.noreply.github.com>
2022-11-27 21:24:21 +0700

    git init

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            var problemDetails = new ProblemDetails
+            {
+                Type = "https://tools.ietf.org/html/rfc72321#section-6.6.1",
+                Title = "An error occerred while processing your request. ",
+                Status = (int)HttpStatusCode.InternalServerError
+            };
+
+            context.Result = new ObjectResult(problemDetails);
+
+
+
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
+ 
---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,24 @@
+using BuberDinner.Api.Filters;
+using BuberDinner.Api.Middleware;
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
+    //options => options.Filters.Add<ErrorHandlingFilterAttribute>()
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
+
+
